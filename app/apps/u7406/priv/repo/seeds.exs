8d56163adca5a1e8d4679e7b0b52b9db9ea478b6
Yoshioka <sss.yoshioka@gmail.com>
2022-11-05 13:13:16 +0000

    🎸 feat: seeds設定

--- app/apps/u7406/priv/repo/seeds.exs
+++ app/apps/u7406/priv/repo/seeds.exs
@@ -9,3 +9,10 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Schemas.Account.User
+alias U7406.Admin
+alias U7406.Repo
+
+Repo.insert!(%User{email: "sss.yoshioka@gmail.com", name: "yux", activated: true})
+Admin.register_admin_user(%{email: "sss.yoshioka@gmail.com", password: "123456"})
