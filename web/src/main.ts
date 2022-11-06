51508e554982db4b2d4ef4e4bbeba514553e76ed
Yoshioka <sss.yoshioka@gmail.com>
2022-11-06 04:28:56 +0000

    🎸 feat: primevue導入

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,5 +2,30 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// import i18n from './services/i18n'
+import PrimeVue from 'primevue/config'
+import ToastService from 'primevue/toastservice'
+import 'primeflex/primeflex.css'
+import 'primeicons/primeicons.css'
+import 'primevue/resources/primevue.css'
+import 'primevue/resources/themes/saga-blue/theme.css'
+import Button from 'primevue/button'
+import Chip from 'primevue/chip'
+import InputText from 'primevue/inputtext'
+import RadioButton from 'primevue/radiobutton'
+import Sidebar from 'primevue/sidebar'
+import Toast from 'primevue/toast'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+  // .use(i18n)
+  .use(router)
+  .use(store)
+  .use(PrimeVue)
+  .use(ToastService)
+  .component('Button', Button)
+  .component('Chip', Chip)
+  .component('InputText', InputText)
+  .component('RadioButton', RadioButton)
+  .component('Sidebar', Sidebar)
+  .component('Toast', Toast)
+  .mount('#app')
