5b50419920a0889bfe7ab91aab564f04aaa36190
Yoshioka <sss.yoshioka@gmail.com>
2022-11-10 00:49:40 +0000

    🎸 feat: .iex.exs更新

--- app/.iex.exs
+++ app/.iex.exs
@@ -8,15 +8,21 @@ import U7406
 IO.puts("* Alias Modules")
 alias U7406.Repo
 alias Schemas.Account.User
-# alias Schemas.Account.Project
-# alias Schemas.Graph.Division
-# alias Schemas.Graph.Edge
-# alias Schemas.Graph.EdgeField
-# alias Schemas.Graph.EdgeType
-# alias Schemas.Graph.Node
-# alias Schemas.Graph.NodeField
-# alias Schemas.Graph.NodeType
-# alias Schemas.Graph.Rule
-# alias Queries.Account
+alias Schemas.Account.Project
+alias Schemas.Calc.Algorithm
+alias Schemas.Graph.Tower
+alias Schemas.Graph.Division
+alias Schemas.Graph.Node
+alias Schemas.Graph.NodeField
+alias Schemas.Graph.NodeType
+alias Schemas.Graph.Edge
+alias Schemas.Graph.EdgeField
+alias Schemas.Graph.EdgeType
+alias Schemas.Graph.Rule
+alias Schemas.Graph.SubGraphFilter
+alias Schemas.Graph.Result
+alias Schemas.Operation.NodesUpload
+alias Schemas.Operation.EdgesUpload
+alias Queries.Account
 
 IO.puts("====-====-====-====-====")
