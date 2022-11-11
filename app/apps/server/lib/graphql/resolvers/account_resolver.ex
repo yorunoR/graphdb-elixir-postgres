e9e11071d9954379a02ba6d0a8d2be454fcba53e
Yoshioka <sss.yoshioka@gmail.com>
2022-11-11 13:33:19 +0000

    💡 refactor: contextからの情報取得をresolverを通して行う

--- app/apps/server/lib/graphql/resolvers/account_resolver.ex
+++ app/apps/server/lib/graphql/resolvers/account_resolver.ex
@@ -1,10 +1,11 @@
 defmodule Graphql.Resolvers.AccountResolver do
   alias Actions.Account.Mutation
+  alias Actions.Account.Query
   alias Schemas.Account.User
 
   def call(action, parent, args, %{context: context}) do
     case action do
-      :signin_user ->
+      action when action in [:signin_user, :user_and_project] ->
         run(action, parent, args, context)
 
       _ ->
@@ -18,6 +19,9 @@ defmodule Graphql.Resolvers.AccountResolver do
 
   def run(action, parent, args, context) do
     case action do
+      :user_and_project ->
+        Query.UserAndProject.run(parent, args, context)
+
       :signin_user ->
         Mutation.SigninUser.run(parent, args, context)
 
