3675d9fc645fcba80141945ff6b6c42bf37e2168
Yoshioka <sss.yoshioka@gmail.com>
2022-11-13 15:17:59 +0000

    🎸 feat: 区画サマリー表示

--- web/src/views/project/division/SummaryView.vue
+++ web/src/views/project/division/SummaryView.vue
@@ -2,12 +2,41 @@
   <main style="max-width: 768px; margin: auto">
     <section>
       <h1>Summary</h1>
+      <div v-if="data">
+        <DataTable
+          :value="data.division.summary"
+          responsive-layout="scroll"
+        >
+          <Column
+            field="key"
+            header="key"
+          />
+          <Column
+            field="val"
+            header="val"
+          />
+        </DataTable>
+      </div>
     </section>
   </main>
 </template>
 
 <script setup lang="ts">
-defineProps<{
+import { useDivisionSummaryQuery } from '@/auto_generated/graphql'
+
+const props = defineProps<{
   divisionId: string;
 }>()
+
+const { data } = useDivisionSummaryQuery({
+  variables: {
+    divisionId: props.divisionId,
+    selections: [
+      'nodeTypeCount',
+      'edgeTypeCount',
+      'nodeCount',
+      'edgeCount'
+    ]
+  }
+})
 </script>
