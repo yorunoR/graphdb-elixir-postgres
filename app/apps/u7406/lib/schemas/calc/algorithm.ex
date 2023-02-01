79d9401910490f428eeb47643ee7030f27e0fa9d
Yoshioka <sss.yoshioka@gmail.com>
2023-02-01 14:25:58 +0000

    🎸 feat: Agent Schema 追加

--- app/apps/u7406/lib/schemas/calc/algorithm.ex
+++ app/apps/u7406/lib/schemas/calc/algorithm.ex
@@ -2,8 +2,13 @@ defmodule Schemas.Calc.Algorithm do
   use U7406.Schema
   import Ecto.SoftDelete.Schema
 
+  alias Schemas.Calc.Agency
+
   schema "algorithms" do
+    belongs_to :agency, Agency
+
     field :name, :string
+    field :arity, :integer
 
     timestamps()
     soft_delete_schema()
