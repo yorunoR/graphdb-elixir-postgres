3473106af726e2d565de7b283f4a075229d4a947
Yoshioka <sss.yoshioka@gmail.com>
2023-02-04 13:58:02 +0000

    🎸 feat: 最短パス Highlight 表示

--- web/src/views/project/sub_graph/DiagramView.vue
+++ web/src/views/project/sub_graph/DiagramView.vue
@@ -3,13 +3,18 @@
     <h1>Diagram</h1>
     <VisDiagramMolecule
       v-if="data"
+      :key="resetKey"
       :subGraphData="data.subGraphData"
+      :results="resultsData ? resultsData.subGraphFilter.results : []"
+      @reset="() => reset()"
     />
   </main>
 </template>
 
 <script setup lang="ts">
-import { useSubGraphDataQuery } from '@/auto_generated/graphql'
+import { ref } from 'vue'
+
+import { useSubGraphDataQuery, useSubGraphFilterResultsQuery } from '@/auto_generated/graphql'
 import VisDiagramMolecule from '@/components/molecules/VisDiagramMolecule.vue'
 
 const props = defineProps<{
@@ -17,8 +22,19 @@ const props = defineProps<{
   subGraphFilterId: string;
 }>()
 
+const resetKey = ref(0)
+
 const { data } = useSubGraphDataQuery({
   variables: { subGraphFilterId: props.subGraphFilterId },
   context: { additionalTypenames: ['SubGraphFilter'] }
 })
+
+const { data: resultsData } = useSubGraphFilterResultsQuery({
+  variables: { subGraphFilterId: props.subGraphFilterId },
+  context: { additionalTypenames: ['Result'] }
+})
+
+const reset = () => {
+  resetKey.value++
+}
 </script>
